198573bd08688b87d259fbfc4a0c7f71ec90f2c7
Sukrita Janseetong <iam_sukrita@hotmail.com>
2020-06-14 21:09:57 +0700

    motion

---
+++ test/unit/Motion/Motion.ino
@@ -0,0 +1,18 @@
+int pir = 3; // เลือกขาอินพุด
+int val = 0;  // ตัวแปรไว้อ่านค่าพิน
+void setup() {
+  Serial.begin(9600);
+  pinMode(pir, INPUT); 
+}
+void loop() {
+    val = digitalRead(pir); // อ่านค่าจากเซนเซอร์ PIR
+    Serial.print("val : ");
+    Serial.println(val); // พิมพ์ค่าที่จับได้จาก PIR
+  if (val == 1) { // ถ้าค่า PIR เป็น 1 แสดงว่าตรวจพบวัตถุ สั่งเปิดไฟ LED
+    //if (digitalRead(pir) == 0) { // ถ้าค่า pir เป็น 0 แสดงว่าตรวจพบวัตถุ สั่งไฟเปิด
+    digitalWrite(pir, 1); //ไฟติด
+  }else {
+    digitalWrite(pir, 0); // ไฟดับ
+  }
+  delay(100);
+} 
