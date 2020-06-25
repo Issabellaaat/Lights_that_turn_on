7c7c8e18820b16bafe4c7a484473bcca7c414a08
Sukrita Janseetong <iam_sukrita@hotmail.com>
2020-06-26 01:44:21 +0700

    OLED

--- test/unit/OLED/OLED.ino
+++ test/unit/OLED/OLED.ino
@@ -4,7 +4,7 @@
 #include <Wire.h>
 
 Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 32, &Wire);
-
+int distance;
 void setup() {
   oled.begin(0x3C); // Address 0x3C for 128x32
 
@@ -21,11 +21,7 @@ void loop() {
   oled.setCursor(0, 0);
   oled.setTextSize(2);
   oled.println("Distance : ");
-  
-  oled.setCursor(0, 21);
-  oled.setTextSize(1);
-  oled.println("1427");
-  
+  oled.println(distance);
   oled.display();
-  delay(10);
+  delay(250); 
 }
